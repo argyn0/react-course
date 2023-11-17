0368f4d4b145f8029d6cf2a149a490ee7b65777f
Ахметов Аргын Ержанович <AkhmetovA@lcoast.itstep>
2023-11-17 19:25:16 +0600

    cw-12:test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import { useSelector, useDispatch } from 'react-redux';
-import {minecraftGetMobsAction} from './store/creators/minecraftCreators'
 import './App.css';
 
 function App() {
-  const selector = useSelector((s) => s.minecraftReducer);
-  const dispatch = useDispatch();
   return (
     <div className="App">
-      <span>{selector.mobs}</span>
-      <button onClick={() => dispatch(minecraftGetMobsAction(["criper", "steave", "zombie"]))}>get mobs</button>
     </div>
   );
 }
