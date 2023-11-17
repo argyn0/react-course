627f14c0bf6f01490afc0fe7e23623ce3e7ccdf4
Ахметов Аргын Ержанович <AkhmetovA@lcoast.itstep>
2023-11-17 19:29:48 +0600

    cw-12: clear-project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// styles
 import './index.css';
 import App from './App';
-// redux
-import store from './store';
-import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
