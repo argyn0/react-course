025d87441176f8a060b1b09d1c28044018598989
argyn0 <argynakhmetov3@gmail.com>
2023-11-22 21:13:59 +0600

    cw-12: not-final

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import s from "./Header.module.scss";
+
+function Header() {
+    return (
+        <div className={s.header}>
+            <h3>Keeper</h3>
+        </div>
+    );
+}
+export default Header;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Button from "../../components/Button";
 
 import s from "./HomePage.module.scss";
+import Header from "../../components/Header";
 
 
 function HomePage() {
     const onBtnClick = () => console.log(null + "a");
     return (
         <>
-            <div>HomePage</div>
+            <Header></Header>
             <Button className={s.button} onClick={onBtnClick}>asd1</Button>
             <Button onClick={onBtnClick}>asd2</Button>
         </>
